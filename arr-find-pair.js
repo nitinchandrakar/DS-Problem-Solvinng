c9252adc50a8d730c859db9a4c07a05a01aad50c
nitin chandrakar <nitin.it90@gmail.com>
2022-06-11 17:59:09 +0530

    find pair sum from given array

---
+++ arr-find-pair.js
@@ -0,0 +1,18 @@
+/*
+Given N array element, check if there exist a pair i,j such that arr[i]+arr[j]==k && i!=j note i & j are index value k is the given sum
+*/
+
+function findPair(arr, k){
+    for(var i=1;i<arr.length;i++){
+        for(j=0;j<i;j++){
+            if(arr[i]+arr[j]==k){
+                return true;
+            }
+        }
+    }
+
+    return false;
+}
+
+
+console.log(findPair([2,1,-4,5,5,2,1,4,0], -1))
