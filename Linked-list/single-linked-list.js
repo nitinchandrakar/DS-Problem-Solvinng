48e79b255eb5ab70080113bb806076991f3efda3
nitin chandrakar <nitin.it90@gmail.com>
2022-06-21 10:23:16 +0530

    linked list complete

--- Linked-list/single-linked-list.js
+++ Linked-list/single-linked-list.js
@@ -1,3 +1,9 @@
+class Node{
+    constructor(value){
+       this.value = value;
+       this.next = null;
+    }
+}
 class SingleLinkList{
     constructor(value){
         this.head = {
@@ -9,16 +15,68 @@ class SingleLinkList{
     }
 
     append(value){
-        const newNode = {
-            value: value,
-            next : null
-        }
+        const newNode = new Node(value)
 
         this.tail.next = newNode;
         this.tail = newNode;
         this.length++;
         return this;
     }
+
+    prepend(value){
+        const newNode = new Node(value);
+        this.newNode.next = this.head;
+        this.head = this.newNode;
+        this.length++;
+        return this;
+    }
+
+    insert(index, value){
+        let currentIndex = 0;
+        let currentNode = this.head;
+        const newNode = new Node(value)
+        while(currentIndex<this.length){
+            if(currentIndex ==  index){
+                newNode.next = currentNode.next;
+                currentNode.next = newNode;
+                this.length++;
+                return this;
+            }
+            currentNode = currentNode.next;
+            currentIndex++;
+        }
+    }
+
+    remove(index){
+        let currentIndex = 0;
+        let prevIndex = index-1;
+        let currentNode = this.head;
+
+        while(prevIndex<this.length){
+            if(currentIndex ==  prevIndex){
+                let removeNode = currentNode.next;
+                currentNode.next = removeNode.next;
+                removeNode = null;
+                this.length--;
+                return this;
+            }
+            currentNode = currentNode.next;
+            currentIndex++;
+        }
+    }
+
+    printList(){
+        let currentNode = this.head;
+        const arr = [];
+        let currentIndex = 0;
+        while(currentIndex<this.length){
+            arr.push(currentNode.value);
+            currentNode = currentNode.next;
+            currentIndex++;
+        }
+        return arr;
+    }
+
 }
 
 const linkedList  = new SingleLinkList(1);
@@ -26,8 +84,9 @@ linkedList.append(2)
 linkedList.append(3)
 linkedList.append(4)
 linkedList.append(5)
-
-console.log(linkedList)
+linkedList.insert(1,10)
+linkedList.remove(1);
+console.log(linkedList.printList())
 
 
 
