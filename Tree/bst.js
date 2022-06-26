2a5399d8f58d3ccefefda7f8383e98c3699c3c39
nitin chandrakar <nitin.it90@gmail.com>
2022-06-27 00:04:18 +0530

    graph implementation

--- Tree/bst.js
+++ Tree/bst.js
@@ -6,54 +6,53 @@ class Node {
   }
 }
 
-class BST{
-  constructor(){
+class BST {
+  constructor() {
     this.root = null;
   }
 
   insert(value) {
     const newNode = new Node(value);
-    
-    if(this.root==null){
-        this.root = newNode;
-    }else{
-        let currentNode = this.root;
-        while(true){
-            if(currentNode.value>value){
-                if(!currentNode.left){
-                    currentNode.left = newNode;
-                    return this;
-                }
-                currentNode = currentNode.left;
-            }else{
-                if(!currentNode.right){
-                    currentNode.right = newNode;
-                    return this;
-                }
-                currentNode = currentNode.right;
-            }
+
+    if (this.root == null) {
+      this.root = newNode;
+    } else {
+      let currentNode = this.root;
+      while (true) {
+        if (currentNode.value > value) {
+          if (!currentNode.left) {
+            currentNode.left = newNode;
+            return this;
+          }
+          currentNode = currentNode.left;
+        } else {
+          if (!currentNode.right) {
+            currentNode.right = newNode;
+            return this;
+          }
+          currentNode = currentNode.right;
         }
+      }
     }
   }
   lookup(value) {
-    if(this.root==null){
-        return null;
+    if (this.root == null) {
+      return null;
     }
 
     let currentNode = this.root;
-    while(currentNode!=null){
-        if(currentNode.value==value){
-            return currentNode;
-        }
-        if(currentNode.value>value){
-            currentNode = currentNode.left;
-        }else{
-            currentNode = currentNode.right;
-        }
+    while (currentNode != null) {
+      if (currentNode.value == value) {
+        return currentNode;
+      }
+      if (currentNode.value > value) {
+        currentNode = currentNode.left;
+      } else {
+        currentNode = currentNode.right;
+      }
     }
 
     return null;
-
   }
 }
 function traverse(node) {
@@ -74,4 +73,4 @@ tree.insert(15);
 tree.insert(1);
 
 //console.log((traverse(tree.root)));
-console.log(tree.lookup(4))
+console.log(tree.lookup(4));
