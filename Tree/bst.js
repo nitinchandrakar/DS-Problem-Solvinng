fb4f50591f2d594314a54262860497bccc86ab92
nitin chandrakar <nitin.it90@gmail.com>
2022-06-26 21:10:35 +0530

    binary search tree

---
+++ Tree/bst.js
@@ -0,0 +1,77 @@
+class Node {
+  constructor(value) {
+    this.left = null;
+    this.right = null;
+    this.value = value;
+  }
+}
+
+class BST{
+  constructor(){
+    this.root = null;
+  }
+
+  insert(value) {
+    const newNode = new Node(value);
+    
+    if(this.root==null){
+        this.root = newNode;
+    }else{
+        let currentNode = this.root;
+        while(true){
+            if(currentNode.value>value){
+                if(!currentNode.left){
+                    currentNode.left = newNode;
+                    return this;
+                }
+                currentNode = currentNode.left;
+            }else{
+                if(!currentNode.right){
+                    currentNode.right = newNode;
+                    return this;
+                }
+                currentNode = currentNode.right;
+            }
+        }
+    }
+  }
+  lookup(value) {
+    if(this.root==null){
+        return null;
+    }
+
+    let currentNode = this.root;
+    while(currentNode!=null){
+        if(currentNode.value==value){
+            return currentNode;
+        }
+        if(currentNode.value>value){
+            currentNode = currentNode.left;
+        }else{
+            currentNode = currentNode.right;
+        }
+    }
+
+    return null;
+
+  }
+}
+function traverse(node) {
+  const tree = { value: node.value };
+  tree.left = node.left == null ? null : traverse(node.left);
+  tree.right = node.right == null ? null : traverse(node.right);
+
+  return tree;
+}
+
+const tree = new BST();
+tree.insert(9);
+tree.insert(4);
+tree.insert(6);
+tree.insert(20);
+tree.insert(170);
+tree.insert(15);
+tree.insert(1);
+
+//console.log((traverse(tree.root)));
+console.log(tree.lookup(4))
