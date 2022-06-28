ed69f16b64cb5144b66b2ddf73a6999d02b21e91
nitin chandrakar <nitin.it90@gmail.com>
2022-06-28 23:58:58 +0530

    BFS implementation on tree

---
+++ Searching-Traversing/bfs.js
@@ -0,0 +1,93 @@
+class Node {
+  constructor(value) {
+    this.left = null;
+    this.right = null;
+    this.value = value;
+  }
+}
+
+class BST {
+  constructor() {
+    this.root = null;
+  }
+
+  insert(value) {
+    const newNode = new Node(value);
+
+    if (this.root == null) {
+      this.root = newNode;
+    } else {
+      let currentNode = this.root;
+      while (true) {
+        if (currentNode.value > value) {
+          if (!currentNode.left) {
+            currentNode.left = newNode;
+            return this;
+          }
+          currentNode = currentNode.left;
+        } else {
+          if (!currentNode.right) {
+            currentNode.right = newNode;
+            return this;
+          }
+          currentNode = currentNode.right;
+        }
+      }
+    }
+  }
+  lookup(value) {
+    if (this.root == null) {
+      return null;
+    }
+
+    let currentNode = this.root;
+    while (currentNode != null) {
+      if (currentNode.value == value) {
+        return currentNode;
+      }
+      if (currentNode.value > value) {
+        currentNode = currentNode.left;
+      } else {
+        currentNode = currentNode.right;
+      }
+    }
+
+    return null;
+  }
+  breadthFirstSearch(){
+    let currentNode = this.root;
+    let queue =[];
+    let list = [];
+    queue.push(currentNode);
+
+    while(queue.length>0){
+        currentNode = queue.shift();
+        list.push(currentNode.value);
+        if(currentNode.left){
+            queue.push(currentNode.left);
+        }
+        if(currentNode.right){
+            queue.push(currentNode.right);
+        }
+    }
+    return list;
+  }
+}
+function traverse(node) {
+  const tree = { value: node.value };
+  tree.left = node.left == null ? null : traverse(node.left);
+  tree.right = node.right == null ? null : traverse(node.right);
+
+  return tree;
+}
+
+const tree = new BST();
+tree.insert(9);
+tree.insert(4);
+tree.insert(6);
+tree.insert(20);
+tree.insert(170);
+tree.insert(15);
+tree.insert(1);
+
+console.log(tree.breadthFirstSearch())
