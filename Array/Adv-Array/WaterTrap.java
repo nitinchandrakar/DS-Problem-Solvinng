92a0c3d257cbd521da5c10da64467432df22de89
nitin chandrakar <nitin.it90@gmail.com>
2023-08-28 23:33:11 +0530

    Adv Array rain water trappinng

---
+++ Array/Adv-Array/WaterTrap.java
@@ -0,0 +1,84 @@
+
+/**
+ * Rain Water Trapped
+ * Problem Description
+Given a vector A of non-negative integers representing an elevation map where the width of each bar is 1, compute how much water it is able to trap after raining.
+
+
+
+Problem Constraints
+1 <= |A| <= 100000
+
+
+
+Input Format
+First and only argument is the vector A
+
+
+
+Output Format
+Return one integer, the answer to the question
+
+
+
+Example Input
+Input 1:
+
+A = [0, 1, 0, 2]
+Input 2:
+
+A = [1, 2]
+
+
+Example Output
+Output 1:
+
+1
+Output 2:
+
+0
+
+
+Example Explanation
+Explanation 1:
+
+1 unit is trapped on top of the 3rd element.
+Explanation 2:
+
+No water is trapped.
+ * 
+*/
+
+import java.util.List;
+
+public class WaterTrap {
+    // DO NOT MODIFY THE LIST. IT IS READ ONLY
+    public int trap(final List<Integer> A) {
+        int N = A.size();
+        int left = 0;
+        int right = N - 1;
+        int lMax = 0, rMax = 0;
+        int water = 0;
+
+        while (left < right) {
+            // maxLeft <= max(maxRight,A[right])
+            if (A.get(left) <= A.get(right)) {
+                if (A.get(left) >= lMax) {
+                    lMax = A.get(left);
+                } else {
+                    water += lMax - A.get(left);
+                }
+                left++;
+            } else {
+                if (A.get(right) >= rMax) {
+                    rMax = A.get(right);
+                } else {
+                    water += rMax - A.get(right);
+                }
+                right--;
+            }
+        }
+
+        return water;
+    }
+}
